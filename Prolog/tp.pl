bafdac9d2206a7784eb9256cba43fbdfcac73ac9
Juan Cruz Basso <juan.basso@mercadolibre.com>
2018-03-11 13:28:09 -0300

    Ejercicio 4

--- Prolog/tp.pl
+++ Prolog/tp.pl
@@ -5,31 +5,42 @@ mapaEjemplo([
       bicisenda(belgrano,retiro,5)]).
 
 %% Ejercicio 1
-%% estaciones(+M, -Es)
+%% estaciones(+B, -Es)
 
-%% estaciones(M, Es):- estaciones2(M,L), sort(L,Es).
-%% estaciones2([],[]).
-%% estaciones2([bicisenda(E1,E2,_)|Bs],[E1,E2|Es]):- estaciones2(Bs,Es).
-
-estaciones(M, Es):- setof(E, estacionConRepetidos(M, E), Es).
+estaciones(B, Es):- setof(E, estacionConRepetidos(B, E), Es).
 
 estacionConRepetidos([bicisenda(E1,_,_)|_], E1).
 estacionConRepetidos([bicisenda(_,E2,_)|_], E2).
 estacionConRepetidos([_|Bs], E):- estacionConRepetidos(Bs, E).
 
+%% Otra implementacion:
+%% estaciones(B, Es):- estaciones2(B,L), sort(L,Es).
+%% estaciones2([],[]).
+%% estaciones2([bicisenda(E1,E2,_)|Bs],[E1,E2|Es]):- estaciones2(Bs,Es).
+
 %% Ejercicio 2
-%% estacionesVecinas(+M,  ?E,  ?Es)
-estacionesVecinas(M, E, Es):- setof(Re, estacionVecina(M, E, Re), Es).
+%% estacionesVecinas(+B,  ?E,  ?Es)
+estacionesVecinas(B, E, Es):- setof(Re, estacionVecina(B, E, Re), Es).
 
-estacionVecina(M, E, Re):- dataDeEstaciones(M, E, Re, _).
+estacionVecina(B, E, Re):- dataDeEstaciones(B, E, Re, _).
 
-dataDeEstaciones(M, E1, E2, D):- dataDeEstaciones2(M, E1, E2, D).
-dataDeEstaciones(M, E1, E2, D):- dataDeEstaciones2(M, E2, E1, D).
-
-dataDeEstaciones2([bicisenda(E1, E2, D)|_], E1, E2, D).
-dataDeEstaciones2([_|Bs], E1, E2, D):- dataDeEstaciones2(Bs, E1, E2, D).
+dataDeEstaciones(B, E1, E2, D):- member(bicisenda(E1,E2,D),B).
+dataDeEstaciones(B, E1, E2, D):- member(bicisenda(E2,E1,D),B).
 
 %% Ejercicio 3
-%% distanciaVecinas(+M, ?E1, ?E2, ?N)
+%% distanciaVecinas(+B, ?E1, ?E2, ?N)
 
-distanciaVecinas(M, E1, E2, D):- dataDeEstaciones(M, E1, E2, D).
+distanciaVecinas(B, E1, E2, D):- dataDeEstaciones(B, E1, E2, D).
+
+%% Ejercicio 4
+%% caminoSimple(+B, +O, +D, ?C)
+caminoSimple(B, O, D, Es):- setof(Ess, caminoSimpleConRepetidos(B, O, D, Ess), Esss), member(Es,Esss).
+
+caminoSimpleConRepetidos(B, E, E, [E]):- dataDeEstaciones(B, E, _, _).
+caminoSimpleConRepetidos(B, O, D, [O| Es]):- dataDeEstaciones(B, O, E, _), borrarTodasLasBicisendasDe(B, O, B2), caminoSimpleConRepetidos(B2, E, D, Es).
+caminoSimpleConRepetidos(B, O, D, [O, D]):- dataDeEstaciones(B, O, D, _).
+
+borrarTodasLasBicisendasDe([], _, []).
+borrarTodasLasBicisendasDe([bicisenda(E1,E2,D)|Bs], E, [bicisenda(E1,E2,D)|Bss]):- E1 \= E, E2 \= E, borrarTodasLasBicisendasDe(Bs, E, Bss).
+borrarTodasLasBicisendasDe([bicisenda(E,_,_)|Bs], E, Bss):- borrarTodasLasBicisendasDe(Bs, E, Bss).
+borrarTodasLasBicisendasDe([bicisenda(_,E,_)|Bs], E, Bss):- borrarTodasLasBicisendasDe(Bs, E, Bss).
